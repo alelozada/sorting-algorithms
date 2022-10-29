63bc0676e5453b2e1f21dc74aab3afe70c232f42
alelozada <77251740+alelozada@users.noreply.github.com>
2022-10-29 00:01:35 -0500

    Update shell_sort.py

--- shell_sort.py
+++ shell_sort.py
@@ -1,4 +1,21 @@
-def shell_sort(arr):
-    ...
+def shell_sort(arr, n):
 
-print(shell_sort([8, 12, 1, 3, 5]))
+    # Rearrange elements at each n/2, n/4, n/8, ... intervals
+    interval = n // 2
+    while interval > 0:
+        for i in range(interval, n):
+            temp = arr[i]
+            j = i
+            while j >= interval and arr[j - interval] > temp:
+                arr[j] = arr[j - interval]
+                j -= interval
+
+            arr[j] = temp
+        interval //= 2
+
+
+data = [8, 12, 1, 3, 5]
+size = len(data)
+shell_sort(data, size)
+print('Sorted Array in Ascending Order:')
+print(data)
